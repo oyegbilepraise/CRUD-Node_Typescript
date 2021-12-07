49c4b931d96b4a5ed22719d46f07a64fd9688c73
Oyegbile Praise <83648252+oyegbilepraise@users.noreply.github.com>
2021-12-07 10:03:57 +0100

    config_url edited

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
     port: 1338,
     host: 'localhost',
-    dbUrl: 'mongodb+srv://oluwadhammieh:07001779@foodapp.t6unl.mongodb.net/crud_ts?retryWrites=true&w=majority'
+    dbUrl: '***'
 }
